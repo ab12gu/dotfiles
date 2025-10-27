65abd990e4ed255665d5f4551788c3ed3e0a2494
Abhay Gupta <ab18gu@gmail.com>
2025-10-26 20:03:02 -0700

    Add lvim and treesitter

--- init.lua
+++ init.lua
@@ -1,14 +1,27 @@
 -- Neovim configuration file
 --
 
+-- add lazy.nvim to runtimepath
+vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
+
+-- now you can call require("lazy")
+require("lazy").setup({
+  -- list your plugins here, e.g., nvim-treesitter
+  --
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+})
+
+require("nvim-treesitter.configs").setup {
+    ensure_installed = {"tsx"},
+    highlight = {enable = true },
+}
 
 -- Reload vimrc in nvim
--- luafile $MYVIMRC 
+-- luafile $MYVIMRC
 -- echo $MYVIMRC -- check filename
 
 -- Show line numbers
 vim.o.number = true
-
 -- existing tabs are 4 spaces
 vim.o.tabstop = 4
 -- On pressing tab, convert tab to spaces
@@ -26,5 +39,3 @@ vim.cmd("colorscheme industry")
 -- Set line number color (api faster than vim-cmd)
 --vim.cmd("highlight LineNr ctermfg=LightBlue")
 vim.api.nvim_set_hl(0, "LineNr", { ctermfg = "LightBlue",})
-
-	
